7101893f1074c78207f76d1d3431a1719857a2e4
melwin-tedd <musictrendiez@gmail.com>
2023-07-22 20:00:40 +0400

    Mobile Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Button from "../Button";
 import localFont from "next/font/local";
+import { CiFacebook, CiInstagram, CiTwitter, CiYoutube } from "react-icons/ci";
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
@@ -49,7 +50,7 @@ function Navbar() {
             : "hidden"
         } `}
       >
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between p-3">
           <div className="flex items-center space-x-3">
             <Image
               src="/images/grand_palace_logo.png"
@@ -58,11 +59,11 @@ function Navbar() {
               alt="Grand Palace General Trading Logo"
             />
             <h2
-              className={`${arialRounded.className} pt-2 text-lg uppercase text-green-900`}
+              className={`${arialRounded.className} pt-2 text-lg uppercase text-white`}
             >
               Grand Palace General Trading
             </h2>
-          </div>{" "}
+          </div>
           <div
             onClick={() => setNavActive(!navActive)}
             className={`flex flex-col gap-1 bg-white p-3 lg:hidden`}
@@ -72,8 +73,8 @@ function Navbar() {
             <div className="h-[3px] w-[25px] bg-green-900"></div>
           </div>
         </div>
-        <div className="mx-auto flex h-full w-max flex-col items-center justify-between gap-6 py-16">
-          <div className=" space-y-6 text-center">
+        <div className="mx-auto flex h-full w-max flex-col items-center justify-between gap-6 py-32">
+          <div className=" space-y-9 text-center">
             {MENU_LIST.map((menu, idx) => (
               <ul
                 onClick={() => {
@@ -81,17 +82,27 @@ function Navbar() {
                   setNavActive(false);
                 }}
                 key={menu.text}
-                className="text-white"
+                className="uppercase text-white"
               >
                 <NavItem active={activeIdx === idx} {...menu} />
               </ul>
             ))}
             <div className="">
-              <h5 className="text-center">Let's Talk</h5>
+              <h5 className="mb-3 text-center font-semibold text-green-300">
+                Let's Talk
+              </h5>
               <Button>Request a Quote</Button>
             </div>
           </div>
-          <h5>Follow us</h5>
+          <div className="text-center">
+            <h5 className="mb-3 font-semibold text-green-300">Follow us</h5>
+            <div className="flex gap-6 text-3xl text-green-100">
+              <CiFacebook className="stroke-[1px]" />
+              <CiInstagram className="stroke-[1px]" />
+              <CiTwitter className="stroke-[1px]" />
+              <CiYoutube className="stroke-[1px]" />
+            </div>
+          </div>
         </div>
       </div>
 
